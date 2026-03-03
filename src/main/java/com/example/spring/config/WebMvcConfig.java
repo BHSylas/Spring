9945d2dce35328826aa24fd2413e0c7e765213a8
reojang <reo7059@naver.com>
2026-03-03 16:01:48 +0900

    수정

--- src/main/java/com/example/spring/config/WebMvcConfig.java
+++ src/main/java/com/example/spring/config/WebMvcConfig.java
@@ -1,46 +1,33 @@
 package com.example.spring.config;
 
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.support.ResourceRegion;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.ResourceRegionHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.List;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    private final Path baseDir;
+
+    public WebMvcConfig(AppProperties props) {
+        this.baseDir = Paths.get(props.getUpload().getBaseDir()).toAbsolutePath().normalize();
+    }
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        String baseLocation = baseDir.toUri().toString();
+
         registry.addResourceHandler("/thumbnails/**")
-                .addResourceLocations("file:///C:/Users/dw-011/Desktop/spring/uploads/thumbnails/");
+                .addResourceLocations(baseLocation + "thumbnails/");
     }
 
     @Bean
     public ResourceRegionHttpMessageConverter resourceRegionHttpMessageConverter() {
         return new ResourceRegionHttpMessageConverter();
     }
-
-    @Bean
-    public ApplicationRunner runner(List<HttpMessageConverter<?>> converters) {
-        return args -> {
-            System.out.println("=== Registered Converters ===");
-            converters.forEach(c ->
-                    {
-                        System.out.println(c.getClass().getName());
-                        if (c instanceof ResourceRegionHttpMessageConverter rr) {
-                            System.out.println("Supports video/mp4: " +
-                                    rr.canWrite(ResourceRegion.class, MediaType.valueOf("video/mp4")));
-                        }
-                    }
-
-            );
-        };
-    }
-
 }
