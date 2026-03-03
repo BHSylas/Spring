6150cd093fbbcf413d286b1ba8aa2c65f9d5ddd0
kuh0701 <kuh0701@naver.com>
2026-03-03 10:52:52 +0900

    섬네일 표출

--- src/main/java/com/example/spring/config/WebMvcConfig.java
+++ src/main/java/com/example/spring/config/WebMvcConfig.java
@@ -7,11 +7,19 @@ import org.springframework.core.io.support.ResourceRegion;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.ResourceRegionHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
-public class WebMvcConfig {
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/thumbnails/**")
+                .addResourceLocations("file:///C:/Users/dw-011/Desktop/spring/uploads/thumbnails/");
+    }
 
     @Bean
     public ResourceRegionHttpMessageConverter resourceRegionHttpMessageConverter() {
@@ -23,13 +31,13 @@ public class WebMvcConfig {
         return args -> {
             System.out.println("=== Registered Converters ===");
             converters.forEach(c ->
-            {
-                System.out.println(c.getClass().getName());
-                if (c instanceof ResourceRegionHttpMessageConverter rr) {
-                    System.out.println("Supports video/mp4: " +
-                            rr.canWrite(ResourceRegion.class, MediaType.valueOf("video/mp4")));
-                }
-            }
+                    {
+                        System.out.println(c.getClass().getName());
+                        if (c instanceof ResourceRegionHttpMessageConverter rr) {
+                            System.out.println("Supports video/mp4: " +
+                                    rr.canWrite(ResourceRegion.class, MediaType.valueOf("video/mp4")));
+                        }
+                    }
 
             );
         };
